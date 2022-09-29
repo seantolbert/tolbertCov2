fadf81d9e8371a46962d1fbd574ff622548e9b47
Sean Tolbert <stolbert4150@gmail.com>
2022-09-29 12:25:36 -0400

    creates projects

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="text-rose-400">
+      <button onClick={() => console.log('higeu')}>neat</button>
+    </div>
+  )
+}
+
+export default App
