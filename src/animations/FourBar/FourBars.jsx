d15b3fca91c0b59f3a8137fafa76178e3e969c4b
Sean Tolbert <stolbert4150@gmail.com>
2022-09-30 18:29:51 -0400

    adds final touches to fourbar

--- src/animations/FourBar/FourBars.jsx
+++ src/animations/FourBar/FourBars.jsx
@@ -51,40 +51,41 @@ export default function FourBars() {
   }, []);
 
   return (
-      <div className=" h-screen overflow-x-hidden relative flex justify-center">
-        <div
-          id="orange"
-          className=" h-full w-fit bg-orange-400 absolute -translate-x-64"
-        >
-          123456
-        </div>
-        <div id="amber" className=" h-full w-fit bg-amber-400 absolute">
-          123456
-        </div>
-        <div id="chosenBar" className=" rotate-[-18deg] w-[2000px] absolute">
-          <StreamBar color={chosen.value} />
-        </div>
-        <div
-          id="red"
-          className="h-full w-fit bg-rose-400 absolute translate-x-32"
-        >
-          123456
-        </div>
-        <div id="highlightBar" className=" rotate-[15deg] w-[2000px] absolute">
-          <StreamBar color={highlight.value} />
-        </div>
-        <div
-          id="orange"
-          className=" h-[440px] w-fit bg-orange-400 absolute -translate-x-64"
-        >
-          123456
-        </div>
-        <div
-          id="green"
-          className="h-full w-fit bg-green-400 absolute -translate-x-32"
-        >
-          123456
-        </div>
+    <div className=" h-screen overflow-x-hidden relative flex justify-center">
+      <div
+        id="orange"
+        className=" h-full w-fit bg-orange-400 absolute -translate-x-64"
+      >
+        123456
       </div>
+      <div id="amber" className=" h-full w-fit bg-amber-400 absolute">
+        123456
+      </div>
+      <div id="chosenBar" className="translate-y-[400px] rotate-[-18deg] w-[2500px] absolute">
+        <StreamBar color={chosen.value} />
+      </div>
+      <div
+        id="red"
+        className="h-full w-fit bg-rose-400 absolute translate-x-32"
+      >
+        123456
+      </div>
+      <div id="highlightBar" className="translate-y-[400px]  rotate-[15deg] w-[2500px] absolute">
+        <StreamBar color={highlight.value} />
+      </div>
+      <div
+        id="orange"
+        className=" h-[420px] w-fit bg-orange-400 absolute -translate-x-64"
+      >
+        123456
+      </div>
+      {/* <div className="z-[9] translate-y-[270px] rounded-full w-56 h-56 bg-purple-400"></div> */}
+      <div
+        id="green"
+        className="z-[10] h-full w-fit bg-green-400 absolute -translate-x-32"
+      >
+        123456
+      </div>
+    </div>
   );
 }
