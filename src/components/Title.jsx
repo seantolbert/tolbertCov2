6318072c39f6cd9dc4436c15eb95e7010a8c9519
Sean Tolbert <stolbert4150@gmail.com>
2022-10-06 14:33:50 -0400

    adds glowing effect to title ring

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -18,54 +18,69 @@ export default function Title() {
   useEffect(() => {
     const tl = gsap.timeline({ defaults: { ease: "power3" } });
     tl.fromTo(
-      "#piece, #moon",
+      "#moon, #piece",
       {
-        x: "-2000px",
+        x: "-400px",
       },
       {
-        ease: "power3",
-        x: "250px",
-        duration: 0.5,
-        opacity: 1,
-      }
-    )
-      .fromTo(
-        "#piece2, #moon2",
-        {
-          x: "-2000px",
-        },
-        {
-          ease: "power3",
-          x: "175px",
-          duration: 0.5,
-          opacity: 1,
-        }
-      )
-      .fromTo(
-        "#piece3, #moon3",
-        {
-          x: "-2000px",
-        },
-        {
-          ease: "power3",
-          x: "100px",
-          duration: 0.5,
-          opacity: 1,
-        }
-      )
-      .to("#titleOne, #titleTwo", {
         duration: 2,
-        ease: "rough",
+        ease: "elastic.out",
         x: 0,
-        opacity: 1,
-      })
-      .to("#piece, #moon, #piece2, #moon2, #piece3, #moon3", {
-        x: "-100px",
-        duration: 1,
-      })
-      .to("#piece, #moon", { x: "1000px", opacity: 0, duration: 0.2 })
-      .to("#piece2, #moon2", { x: "1000px", opacity: 0, duration: 0.2 })
-      .to("#piece3, #moon3", { x: "1000px", opacity: 0, duration: 0.2 });
+      }
+    ).to("#moon, #piece", {
+      duration: 1.5,
+      boxShadow: "0px 0px 105px 45px rgba(252,211,77,0.9)",
+    });
+
+    // tl.fromTo(
+    //   "#piece, #moon",
+    //   {
+    //     x: "-2000px",
+    //   },
+    //   {
+    //     ease: "power3",
+    //     x: "250px",
+    //     duration: 0.5,
+    //     opacity: 1,
+    //   }
+    // )
+    //   .fromTo(
+    //     "#piece2, #moon2",
+    //     {
+    //       x: "-2000px",
+    //     },
+    //     {
+    //       ease: "power3",
+    //       x: "175px",
+    //       duration: 0.5,
+    //       opacity: 1,
+    //     }
+    //   )
+    //   .fromTo(
+    //     "#piece3, #moon3",
+    //     {
+    //       x: "-2000px",
+    //     },
+    //     {
+    //       ease: "power3",
+    //       x: "100px",
+    //       duration: 0.5,
+    //       opacity: 1,
+    //     }
+    //   )
+    //   .to("#titleOne, #titleTwo", {
+    //     duration: 2,
+    //     ease: "rough",
+    //     x: 0,
+    //     opacity: 1,
+    //   })
+    //   .to("#piece, #moon, #piece2, #moon2, #piece3, #moon3", {
+    //     x: "-100px",
+    //     duration: 1,
+    //   })
+    //   .to("#piece, #moon", { x: "1000px", opacity: 0, duration: 0.2 })
+    //   .to("#piece2, #moon2", { x: "1000px", opacity: 0, duration: 0.2 })
+    //   .to("#piece3, #moon3", { x: "1000px", opacity: 0, duration: 0.2 });
   }, []);
 
   // note! what if there was a cool pulsing ring effect on repeat where
@@ -78,40 +93,40 @@ export default function Title() {
         <div className="flex flex-col gap-2 items-start justify-center h-full w-full">
           <div
             id="moon"
-            className="border-[20px] border-t-amber-500 border-r-amber-500 border-b-amber-500 border-l-transparent absolute w-[400px] h-[400px] rounded-full opacity-0"
+            className="scale-x-50 border-[20px] border-t-amber-300  border-r-amber-300  border-b-amber-300  border-l-transparent absolute w-[400px] h-[400px] rounded-full"
           ></div>
-          <div
+          {/* <div
             id="moon2"
-            className="border-[20px] border-t-amber-500 border-r-amber-500 border-b-amber-500 border-l-transparent absolute w-[400px] h-[400px] rounded-full opacity-0"
+            className="border-[20px] border-t-amber-300  border-r-amber-300  border-b-amber-300  border-l-transparent absolute w-[400px] h-[400px] rounded-full opacity-0"
           ></div>
           <div
             id="moon3"
-            className="border-[20px] border-t-amber-500 border-r-amber-500 border-b-amber-500 border-l-transparent absolute w-[400px] h-[400px] rounded-full opacity-0"
-          ></div>
+            className="border-[20px] border-t-amber-300  border-r-amber-300  border-b-amber-300  border-l-transparent absolute w-[400px] h-[400px] rounded-full opacity-0"
+          ></div> */}
           <p
             id="titleOne"
-            className="p-2 font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-rose-500 to-cyan-500 text-9xl opacity-0 -translate-x-[100%]"
+            className="p-2 font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-rose-500 to-cyan-500 text-9xl z-[1]"
           >
             Hey! my name is Sean
           </p>
           <p
             id="titleTwo"
-            className="p-2 font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-cyan-500 to-rose-500 text-9xl opacity-0 -translate-x-[100%]"
+            className="p-2 font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-cyan-500 to-rose-500 text-9xl z-[1]"
           >
             Come say Hi
           </p>
           <div
             id="piece"
-            className="border-[20px] border-l-amber-500 border-y-transparent border-r-transparent absolute w-[400px] h-[400px] rounded-full opacity-0"
+            className="z-[2] scale-x-50 border-[20px] border-l-amber-300 border-y-transparent border-r-transparent absolute w-[400px] h-[400px] rounded-full"
           ></div>
-          <div
+          {/* <div
             id="piece2"
-            className="border-[20px] border-l-amber-500 border-y-transparent border-r-transparent absolute w-[400px] h-[400px] rounded-full opacity-0"
-          ></div>
-          <div
+            className="border-[20px] border-l-amber-300  border-y-transparent border-r-transparent absolute w-[400px] h-[400px] rounded-full opacity-0"
+          ></div> */}
+          {/* <div
             id="piece3"
-            className="border-[20px] border-l-amber-500 border-y-transparent border-r-transparent absolute w-[400px] h-[400px] rounded-full opacity-0"
-          ></div>
+            className="border-[20px] border-l-amber-300  border-y-transparent border-r-transparent absolute w-[400px] h-[400px] rounded-full opacity-0"
+          ></div> */}
         </div>
       </main>
     </>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -13,6 +13,7 @@ module.exports = {
         pressDark: "inset -9px 9px 18px #2d2d2d, inset 9px -9px 18px #616161",
         light: "-9px 9px 18px #9c9c9c, 9px -9px 18px #ffffff",
         pressLight: "inset -9px 9px 18px #9c9c9c, inset 9px -9px 18px #ffffff",
+        ringGlow: "0px 0px 105px 45px rgba(252,211,77,0.9);"
       },
     },
   },
