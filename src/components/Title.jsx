d20f83b3faee179d53b26a1e7959abbd7012c22d
Sean Tolbert <stolbert4150@gmail.com>
2022-10-05 22:16:20 -0400

    adds some ring animation to title

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -16,25 +16,78 @@ export default function Title() {
   }, [theme.value]);
 
   useEffect(() => {
-    const tl = gsap.timeline()
-    gsap.to("#titleOne, #titleTwo", {
-      duration: 2,
-      ease: "rough",
-      x: 0,
-      opacity: 1,
-    });
-
-    tl.to("#piece, #moon", {
-      duration: 1,
-      
-    })
+    const tl = gsap.timeline({ defaults: { ease: "power3" } });
+    tl.fromTo(
+      "#piece, #moon",
+      {
+        x: "-2000px",
+      },
+      {
+        ease: "power3",
+        x: "250px",
+        duration: 0.5,
+        opacity: 1,
+      }
+    )
+      .fromTo(
+        "#piece2, #moon2",
+        {
+          x: "-2000px",
+        },
+        {
+          ease: "power3",
+          x: "175px",
+          duration: 0.5,
+          opacity: 1,
+        }
+      )
+      .fromTo(
+        "#piece3, #moon3",
+        {
+          x: "-2000px",
+        },
+        {
+          ease: "power3",
+          x: "100px",
+          duration: 0.5,
+          opacity: 1,
+        }
+      )
+      .to("#titleOne, #titleTwo", {
+        duration: 2,
+        ease: "rough",
+        x: 0,
+        opacity: 1,
+      })
+      .to("#piece, #moon, #piece2, #moon2, #piece3, #moon3", {
+        x: "-100px",
+        duration: 1,
+      })
+      .to("#piece, #moon", { x: "1000px", opacity: 0, duration: 0.2 })
+      .to("#piece2, #moon2", { x: "1000px", opacity: 0, duration: 0.2 })
+      .to("#piece3, #moon3", { x: "1000px", opacity: 0, duration: 0.2 });
   }, []);
+
+  // note! what if there was a cool pulsing ring effect on repeat where
+  // these rings go over the words from the left as they are (gold rings ofc) and they faded away
+  // before hitting the end of the screen
 
   return (
     <>
-      <main className="h-screen text-black dark:text-white  w-screen">
+      <main className="h-screen text-black dark:text-white w-screen overflow-hidden">
         <div className="flex flex-col gap-2 items-start justify-center h-full w-full">
-          <div id="moon" className="border-[20px] border-t-amber-500 border-r-amber-500 border-b-amber-500 border-l-transparent absolute w-[400px] h-[400px] rounded-full"></div>
+          <div
+            id="moon"
+            className="border-[20px] border-t-amber-500 border-r-amber-500 border-b-amber-500 border-l-transparent absolute w-[400px] h-[400px] rounded-full opacity-0"
+          ></div>
+          <div
+            id="moon2"
+            className="border-[20px] border-t-amber-500 border-r-amber-500 border-b-amber-500 border-l-transparent absolute w-[400px] h-[400px] rounded-full opacity-0"
+          ></div>
+          <div
+            id="moon3"
+            className="border-[20px] border-t-amber-500 border-r-amber-500 border-b-amber-500 border-l-transparent absolute w-[400px] h-[400px] rounded-full opacity-0"
+          ></div>
           <p
             id="titleOne"
             className="p-2 font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-rose-500 to-cyan-500 text-9xl opacity-0 -translate-x-[100%]"
@@ -47,7 +100,18 @@ export default function Title() {
           >
             Come say Hi
           </p>
-          <div id="piece" className="border-[20px] border-l-amber-500 border-y-transparent border-r-transparent absolute w-[400px] h-[400px] rounded-full"></div>
+          <div
+            id="piece"
+            className="border-[20px] border-l-amber-500 border-y-transparent border-r-transparent absolute w-[400px] h-[400px] rounded-full opacity-0"
+          ></div>
+          <div
+            id="piece2"
+            className="border-[20px] border-l-amber-500 border-y-transparent border-r-transparent absolute w-[400px] h-[400px] rounded-full opacity-0"
+          ></div>
+          <div
+            id="piece3"
+            className="border-[20px] border-l-amber-500 border-y-transparent border-r-transparent absolute w-[400px] h-[400px] rounded-full opacity-0"
+          ></div>
         </div>
       </main>
     </>
